bdab6db10c6f6ba52ad2a9ad0d0b670c0613e6a1
nikolai-petukhov <nikolaipetukhov@gmai.com>
2023-01-19 18:45:08 +0300

    create a simple frontend for display list of companies types

--- src/main/companies-frontend/src/App.js
+++ src/main/companies-frontend/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import companyTypeService from './services/companyType'
 
-function App() {
+const App = () => {
+  const [companyTypes, setcompanyTypes] = useState([])
+
+  useEffect(() => {
+    companyTypeService.getAll().then((compTypes) => setcompanyTypes(compTypes))
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>Company Types Catalog</h1>
+      <ul>
+        {companyTypes.map(companyType => <li key={companyType.id}>{companyType.typeName}</li>)}
+      </ul>
+    </>
+  )
 }
 
-export default App;
+export default App
