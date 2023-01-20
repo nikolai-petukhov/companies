27f118cb2f42888e70cf50a68c47d289846bb24b
nikolai-petukhov <nikolaipetukhov@gmai.com>
2023-01-20 22:24:10 +0300

    finish creating of endpoint for companyType entity

--- src/main/java/com/example/companies/controller/CompanyTypeController.java
+++ src/main/java/com/example/companies/controller/CompanyTypeController.java
@@ -3,6 +3,7 @@ package com.example.companies.controller;
 import com.example.companies.exceptions.CompanyTypeNotFoundException;
 import com.example.companies.model.CompanyType;
 import com.example.companies.repository.CompanyTypeRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,9 +22,29 @@ public class CompanyTypeController {
         return this.companyTypeRepository.findAll();
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("{id}")
     public CompanyType getCompanyTypeById(@PathVariable("id") Long id) {
         return companyTypeRepository.findById(id)
                 .orElseThrow(() -> new CompanyTypeNotFoundException(id));
+    }
+
+    @PostMapping()
+    public CompanyType newCompanyType(@RequestBody CompanyType companyType) {
+        return companyTypeRepository.save(companyType);
+    }
+
+    @PutMapping("{id}")
+    public CompanyType updateCompanyType(
+            @PathVariable("id") CompanyType companyTypeFromDb,
+            @RequestBody CompanyType companyType
+    ) {
+        BeanUtils.copyProperties(companyType, companyTypeFromDb, "id");
+        return companyTypeRepository.save(companyTypeFromDb);
+    }
+
+
+    @DeleteMapping("{id}")
+    public void deleteCompanyType(@PathVariable("id") Long id) {
+        companyTypeRepository.deleteById(id);
     }
 }
