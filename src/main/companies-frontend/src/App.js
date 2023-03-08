162606e22f9ed1328b49e6a5ff6ebf9685f4879c
nikolai-petukhov <nikolaipetukhov@gmai.com>
2023-03-08 18:08:49 +0300

    FE: create the output of all catalogs

--- src/main/companies-frontend/src/App.js
+++ src/main/companies-frontend/src/App.js
@@ -1,18 +1,35 @@
 import React, { useState, useEffect } from 'react'
 import companyTypeService from './services/companyType'
+import companyService from './services/company'
+import employeeService from './services/employee'
 
 const App = () => {
-  const [companyTypes, setcompanyTypes] = useState([])
+  const [companyTypes, setCompanyTypes] = useState([])
+  const [companies, setCompanies] = useState([])
+  const [employees, setEmployees] = useState([])
 
   useEffect(() => {
-    companyTypeService.getAll().then((compTypes) => setcompanyTypes(compTypes))
+    companyTypeService.getAll().then((companyTypesList) => setCompanyTypes(companyTypesList))
+    companyService.getAll().then(companiesList => setCompanies(companiesList))
+    employeeService.getAll().then(employeesList => setEmployees(employeesList))
   }, [])
 
   return (
     <>
-      <h1>Company Types Catalog</h1>
+      <h1>Company catalog</h1>
+      <ul>
+        {companies.map(company => <li key={company.id}>{company.name}</li>)}
+      </ul>
+
+
+      <h2>Company Types Catalog</h2>
       <ul>
         {companyTypes.map(companyType => <li key={companyType.id}>{companyType.typeName}</li>)}
+      </ul>
+
+      <h2>Employees Catalog</h2>
+      <ul>
+        {employees.map(employee => <li key={employee.id}>{employee.firstName} {employee.lastName}</li>)}
       </ul>
     </>
   )
