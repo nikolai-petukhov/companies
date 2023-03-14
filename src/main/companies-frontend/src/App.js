3a42d779ed72847d7b2d09045cfe4a51e69a509c
nikolai-petukhov <nikolaipetukhov@gmai.com>
2023-03-14 19:36:26 +0300

    fe:create tabs menu

--- src/main/companies-frontend/src/App.js
+++ src/main/companies-frontend/src/App.js
@@ -1,37 +1,14 @@
-import React, { useState, useEffect } from 'react'
-import companyTypeService from './services/companyType'
-import companyService from './services/company'
-import employeeService from './services/employee'
+import React from 'react'
+
+import { Container }  from '@mui/material'
+
+import TabMenu from './components/tab-menu'
 
 const App = () => {
-  const [companyTypes, setCompanyTypes] = useState([])
-  const [companies, setCompanies] = useState([])
-  const [employees, setEmployees] = useState([])
-
-  useEffect(() => {
-    companyTypeService.getAll().then((companyTypesList) => setCompanyTypes(companyTypesList))
-    companyService.getAll().then(companiesList => setCompanies(companiesList))
-    employeeService.getAll().then(employeesList => setEmployees(employeesList))
-  }, [])
-
   return (
-    <>
-      <h1>Company catalog</h1>
-      <ul>
-        {companies.map(company => <li key={company.id}>{company.name}</li>)}
-      </ul>
-
-
-      <h2>Company Types Catalog</h2>
-      <ul>
-        {companyTypes.map(companyType => <li key={companyType.id}>{companyType.typeName}</li>)}
-      </ul>
-
-      <h2>Employees Catalog</h2>
-      <ul>
-        {employees.map(employee => <li key={employee.id}>{employee.firstName} {employee.lastName}</li>)}
-      </ul>
-    </>
+    <Container maxWidth="lg">
+      <TabMenu></TabMenu>
+    </Container>
   )
 }
 
